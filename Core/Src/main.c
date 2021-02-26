46888ca7846499d3ad01cb5f724a425887dc2524
Rinaldo Dos Santos <rinaldo@sinteck.com>
2021-02-26 17:17:29 -0300

    Add Example Label

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -46,7 +46,7 @@ uint8_t buf_tft[LV_HOR_RES_MAX*LV_VER_RES_MAX] = {0};
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+void lv_ex_label_1(void);
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -120,6 +120,8 @@ int main(void)
    disp_drv.flush_cb = ST7735_Flush;	//Set your driver function
    disp_drv.buffer = &disp_buf;          //Assign the buffer to teh display
    lv_disp_drv_register(&disp_drv);      //Finally register the driver
+
+   lv_ex_label_1();
    /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -191,7 +193,23 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+void lv_ex_label_1(void)
+{
+    lv_obj_t * label1 = lv_label_create(lv_scr_act(), NULL);
+    lv_label_set_long_mode(label1, LV_LABEL_LONG_BREAK);     /*Break the long lines*/
+    lv_label_set_recolor(label1, true);                      /*Enable re-coloring by commands in the text*/
+    lv_label_set_align(label1, LV_LABEL_ALIGN_CENTER);       /*Center aligned lines*/
+    lv_label_set_text(label1, "#0000ff Re-color# #ff00ff words# #ff0000 of a# label "
+                              "and  wrap long text automatically.");
+    lv_obj_set_width(label1, 150);
+    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, -30);
 
+    lv_obj_t * label2 = lv_label_create(lv_scr_act(), NULL);
+    lv_label_set_long_mode(label2, LV_LABEL_LONG_SROLL_CIRC);     /*Circular scroll*/
+    lv_obj_set_width(label2, 150);
+    lv_label_set_text(label2, "It is a circularly scrolling text. ");
+    lv_obj_align(label2, NULL, LV_ALIGN_CENTER, 0, 30);
+}
 /* USER CODE END 4 */
 
  /**
